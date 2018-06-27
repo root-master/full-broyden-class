1f020ea3710886527c41675bad8ef97c422b9e20
Jacob Rafati <yrafati@gmail.com>
2018-06-27 00:30:35 -0700

    fixed a bug

--- FullBroydenClass.py
+++ FullBroydenClass.py
@@ -625,7 +625,13 @@ class FullBroydenClass:
 		new_y = self.model.eval_y(use_overlap=self.use_overlap)
 		new_s = p
 		if new_s.T @ new_y <= 0 and self.quasi_Newton_matrix == 'L_BFGS':
-			print('s * y <= 0 ==> danger zone') 
+			print('curvature condition did not satisfy for L_BFGS ==> danger zone') 
+			alpha = self.satisfy_curvature_condition(p)
+			# alpha = self.satisfy_wolfe_condition(p)
+			new_s = alpha * p
+			new_loss = self.model.eval_aux_loss(p_vec=alpha * p) 
+			new_y = self.model.eval_y(use_overlap=self.use_overlap)
+
 		self.update_S_Y(new_s,new_y)
 		self.update_M()
 		self.iter += 1
